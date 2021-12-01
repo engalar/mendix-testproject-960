481cfd4e45d06f2757e05422a6e15bd817e02918
engalar <engalar@gmail.com>
2021-12-01 13:39:19 +0800

    first commit

---
+++ deployment/native/styles.js
@@ -0,0 +1,9 @@
+import * as style0 from "C:/d/mendix/960/themesource/nanoflowcommons/native/main";
+import * as style1 from "C:/d/mendix/960/themesource/webactions/native/main";
+import * as style2 from "C:/d/mendix/960/themesource/atlas_core/native/main";
+import * as style3 from "C:/d/mendix/960/themesource/myfirstmodule/native/main";
+import * as style4 from "C:/d/mendix/960/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4]);
